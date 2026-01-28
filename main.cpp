9df683e51de4c1162f4c75f4028ee1de543d0582
ileaheichelberger <ileah.eichelberger@student.northampton.edu>
2026-01-28 19:01:08 +0000

    BMI works

--- main.cpp
+++ main.cpp
@@ -7,9 +7,13 @@ int height, weight;
  height = 61;
  weight = 130; 
 
- double kg, meters;
+ double kg, meters,bmi;
 
  kg = weight*0.453592;
- meters = height*0.0254
+ meters = height*0.0254;
 
+ bmi = kg/(meters*meters);
+
+ cout << "The BMI for this person is: << bmi << endl";
+ 
 }
